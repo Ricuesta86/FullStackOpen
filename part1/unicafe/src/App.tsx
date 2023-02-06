d91e05bebce7b6ea790ef23351aeac739f7ed8c0
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2023-02-06 13:14:33 -0500

    unicafe paso 2

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
   const handleGood = () => setGood(good + 1);
   const handleNeutral = () => setNeutral(neutral + 1);
@@ -19,6 +20,8 @@ function App() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>positives {(good / (good + neutral + bad)) * 100}</p>
     </div>
   );
 }
