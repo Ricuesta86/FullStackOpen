78dd4a55cdd4f352b655014017fd95514a80bb8f
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2023-02-06 13:42:35 -0500

    unicafe paso 5

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -1,29 +1,22 @@
 import { useState } from "react";
 
-const Statistics = ({ good, neutral, bad }: any) => {
-  if (good === 0 && neutral === 0 && bad === 0)
-    return (
-      <>
-        <p>No feedback given</p>
-      </>
-    );
+const Statistics = ({ text, value }: any) => {
   return (
     <>
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>positives {good !== 0 ? (good / (good + neutral + bad)) * 100 : 0}</p>
+      <p>
+        {text} {value}
+      </p>
     </>
   );
 };
+const Button = ({ handle, text }: any) => (
+  <button onClick={handle}>{text}</button>
+);
 
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
 
   const handleGood = () => setGood(good + 1);
   const handleNeutral = () => setNeutral(neutral + 1);
@@ -32,10 +25,24 @@ function App() {
   return (
     <div className="App">
       <h1>Give Feedback</h1>
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Button handle={handleGood} text={"good"} />
+      <Button handle={handleNeutral} text={"neutral"} />
+      <Button handle={handleBad} text={"bad"} />
+      <h2>Statistics</h2>
+      {good === 0 && neutral === 0 && bad === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <Statistics text={"good"} value={good} />
+          <Statistics text={"neutral"} value={neutral} />
+          <Statistics text={"bad"} value={bad} />
+          <Statistics text={"all"} value={good + neutral + bad} />
+          <Statistics
+            text={"positives"}
+            value={good !== 0 ? (good / (good + neutral + bad)) * 100 : 0}
+          />
+        </>
+      )}
     </div>
   );
 }
