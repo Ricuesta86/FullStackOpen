aa1075a9a535c1e1e157b8bcaaeadf32ee1cb701
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2023-02-07 06:13:20 -0500

    anecdota paso 2

--- part1/anecdotas/src/App.tsx
+++ part1/anecdotas/src/App.tsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
+
 const getRandomInt = (max: any) => {
   return Math.floor(Math.random() * max);
 };
+
+const getArrayCero = (length:Number) =>{
+  return Array.apply(null, new Array(length)).map(Number.prototype.valueOf,0);
+}
 const App = ({ anecdotes }: any) => {
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(getArrayCero(anecdotes.length))
+
   const handleClick = () => {
-    setSelected(getRandomInt(anecdotes.length - 1));
+    setSelected(getRandomInt(anecdotes.length));
+  };
+
+  const handleAddVotes = (selected:any) => {
+    console.log(points[selected])
+    setPoints(points=>
+      [
+        ...points,
+        points[selected]+=1
+      ]
+    )
   };
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
+      <p>{anecdotes[selected]} {points[selected]} votes</p>
+      <button onClick={()=>handleAddVotes(selected)}>vote</button>
       <button onClick={handleClick}>Next Anecdote</button>
     </div>
   );
